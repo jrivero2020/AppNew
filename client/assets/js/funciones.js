b0b28ace3591a158204e778e94ff870e3fbe74cd
jrivero2020 <67881282+jrivero2020@users.noreply.github.com>
2025-05-08 13:00:46 -0400

    01-Reconstrucción para Linux

--- client/assets/js/funciones.js
+++ client/assets/js/funciones.js
@@ -2,9 +2,9 @@
 // Define si se muestra o no la etiqueta en el browser
 //
 const chkActivoRolAutentica = (data, isAuthenticated, jwtRol) => {
-  let ret = false;
+  // let ret = false;
   const objeto = data.datos;
-
+/*
   if (objeto === undefined) {
     return;
   }
@@ -15,6 +15,9 @@ const chkActivoRolAutentica = (data, isAuthenticated, jwtRol) => {
     if (objeto.autorizado === "A" && isAuthenticated) ret = true;
     if (objeto.autorizado === "A1" && isAuthenticated && jwtRol === 1)
       ret = true;
+    if (objeto.autorizado === "A9" && isAuthenticated && jwtRol === 9)
+      ret = true;
+
     if (
       objeto.autorizado === "A12" &&
       isAuthenticated &&
@@ -24,5 +27,42 @@ const chkActivoRolAutentica = (data, isAuthenticated, jwtRol) => {
   }
 
   return ret;
+  */
+
+  if (!objeto || objeto.activo !== 1) return false;
+  if (jwtRol === 1) return true; // Admin siempre tiene acceso.
+
+  const { autorizado } = objeto;
+  if (objeto.autorizado === 1) return true;
+  
+  if (autorizado === "!A" && !isAuthenticated) return true; // Todos pueden acceder.  
+  if (autorizado === "A" && isAuthenticated) return true; // Cualquier autenticado puede acceder.
+
+  if (autorizado.startsWith("A")) {
+    if (!isAuthenticated) return false; // Si requiere autenticación y no está autenticado, denegar acceso.
+
+    const rolesPermitidos = new Set(
+      autorizado.slice(1).split("").map(Number) // Extrae los números después de "A"
+    );
+console.log("rolesPermitidos=>:", rolesPermitidos, " jwtRol=>:", jwtRol)
+    return rolesPermitidos.has(jwtRol);
+  }
+
+  if ( !isAuthenticated && autorizado.startsWith("!A")) {
+    
+    const rolesPermitidos = new Set(
+      autorizado.slice(2).split("").map(Number) // Extrae los números después de "!A"
+    );
+
+    return rolesPermitidos.has(jwtRol) //  || isAuthenticated; 
+    // Si tiene rol permitido o está autenticado (acceso a todo lo de "!A").
+  }
+
+  return false;
+
+
+
+
+
 };
 export { chkActivoRolAutentica };
