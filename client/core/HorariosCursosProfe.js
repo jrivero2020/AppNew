4b492cb3b84662c0444c3881719518cc1885475a
jrivero2020 <jrivero.2005@gmail.com>
2025-05-18 01:14:14 -0400

    antes de aplicar timepicker

--- client/core/HorariosCursosProfe.js
+++ client/core/HorariosCursosProfe.js
@@ -30,7 +30,7 @@ import {
 } from "../docentes/api-docentes";
 
 import { AuthContext } from "./AuthProvider";
-// ../core/AuthProvider";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const AsignacionCompleta = () => {
   const [cursosDisponibles, setCursosDisponibles] = useState([]);
@@ -50,7 +50,18 @@ const AsignacionCompleta = () => {
   const usrRol = jwt.user._rol;
   const abortController = new AbortController();
   const signal = abortController.signal;
-
+  const themeLocal = createTheme({
+    palette: {
+      primary: {
+        main: "#1976d2", // Este es el azul estándar de Material-UI
+        light: "#42a5f5",
+        dark: "#00008b",
+      },
+      secondary: {
+        main: "#9c27b0",
+      },
+    },
+  });
   useEffect(() => {
     const fetchData = async () => {
       console.log("Entrando en fetchData****");
@@ -82,23 +93,33 @@ const AsignacionCompleta = () => {
         setDiasDisponibles(diasRes);
         setCursosSeleccionados(cursosArray.map((c) => c.id_curso));
         // Al obtener los datos:
-const horariosFiltrados = horariosArray.map(({ id_dia, dia, desde, hasta }) => ({
-  id_dia,
-  dia,
-  desde,
-  hasta
-}));
+        const horariosFiltrados = horariosArray.map(
+          ({ id_dia, dia, desde, hasta }) => ({
+            id_dia,
+            dia,
+            desde,
+            hasta,
+          })
+        );
 
-const horariosUnicos = horariosFiltrados.filter((horario, index, self) => 
-  index === self.findIndex(h => 
-    h.id_dia === horario.id_dia &&
-    h.dia === horario.dia &&
-    h.desde === horario.desde &&
-    h.hasta === horario.hasta
-  )
-);
+        const horariosUnicos = horariosFiltrados.filter(
+          (horario, index, self) =>
+            index ===
+            self.findIndex(
+              (h) =>
+                h.id_dia === horario.id_dia &&
+                h.dia === horario.dia &&
+                h.desde === horario.desde &&
+                h.hasta === horario.hasta
+            )
+        );
 
-console.log( "horariosArray:", horariosArray, "  horariosUnicos:",horariosUnicos)
+        console.log(
+          "horariosArray:",
+          horariosArray,
+          "  horariosUnicos:",
+          horariosUnicos
+        );
         setHorarios(
           horariosUnicos.map((h) => ({
             id_dia: h.id_dia,
@@ -180,166 +201,256 @@ console.log( "horariosArray:", horariosArray, "  horariosUnicos:",horariosUnicos
   }
 
   return (
-    <Container maxWidth="lg"  rowSpacing={1} columnSpacing={2} sx={{ mt:12 }}>
-      <Typography
-        sx={{
-          fontWeight: "bold",
-          color: "blue",
-          textAlign: "center", // Centra el texto dentro de Typography
-          mt: 1,
-        }}
+    <ThemeProvider theme={themeLocal}>
+      <Container
+        maxWidth={false}
+        sx={{ display: "flex", justifyContent: "center" }}
       >
-        Asignar Curso: {nombreProfesor}
-      </Typography>
-      <Card sx={{ mb: 4 }}>
-        <CardHeader
-          title="Cursos Asignados"
-          titleTypographyProps={{ variant: "h6" }}
-          sx={{ bgcolor: "primary.main", color: "white" }}
-        />
-        <CardContent>
-          <Grid container rowSpacing={0.25} columnSpacing={2} >
-            {cursosDisponibles.map((curso) => (
-              <Grid item xs={12} sm={6} md={4} key={curso.id_curso}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={cursosSeleccionados.includes(curso.id_curso)}
-                      onChange={() => handleCursoChange(curso.id_curso)}
-                    />
-                  }
-                  label={`${curso.nomlargo}`}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ mb: 4 }}>
-        <CardHeader
-          title="Horarios de Atención"
-          titleTypographyProps={{ variant: "h6" }}
-          sx={{ bgcolor: "primary.main", color: "white" }}
-        />
-        <CardContent>
-          <Grid container spacing={2} sx={{ mb: 2 }}>
-            <Grid item xs={12} md={3}>
-              <TextField
-                select
-                fullWidth
-                label="Día"
-                name="id_dia"
-                value={nuevoHorario.id_dia}
-                onChange={handleHorarioChange}
-                required
+        <Grid
+          container
+          maxWidth="lg"
+          rowSpacing={1}
+          columnSpacing={2}
+          sx={{ mt: 12 }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              color: "primary.dark",
+              textAlign: "center", // Centra el texto horizontalmente
+              width: "100%", // Asegura que ocupe todo el ancho disponible
+              mt: 1,
+            }}
+          >
+            Asignar Curso: {nombreProfesor}
+          </Typography>
+          <Card sx={{ mb: 4, justifyContent: "center", alignItems: "center" }}>
+            <CardHeader
+              title="Cursos Asignados"
+              titleTypographyProps={{ variant: "h6" }}
+              sx={{ bgcolor: "primary.main", color: "white" }}
+            />
+            <CardContent>
+              <Grid
+                container
+                rowSpacing={0.25}
+                columnSpacing={2}
+                sx={{
+                  justifyContent: "center", // Centra horizontalmente
+                  alignItems: "center", // Centra verticalmente (si es necesario)
+                }}
               >
-                <MenuItem value="">Seleccione día</MenuItem>
-                {diasDisponibles.map((dia) => (
-                  <MenuItem key={dia.id} value={dia.id}>
-                    {dia.nombre_dia}
-                  </MenuItem>
+                {cursosDisponibles.map((curso) => (
+                  <Grid item xs={12} sm={6} md={3} key={curso.id_curso}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={cursosSeleccionados.includes(curso.id_curso)}
+                          onChange={() => handleCursoChange(curso.id_curso)}
+                        />
+                      }
+                      label={`${curso.nomlargo}`}
+                      sx={{
+                        // Estilo cuando está seleccionado
+                        "& .MuiFormControlLabel-label": {
+                          color: cursosSeleccionados.includes(curso.id_curso)
+                            ? "primary.main"
+                            : "inherit", // Color por defecto si no está seleccionado
+                          fontWeight: cursosSeleccionados.includes(
+                            curso.id_curso
+                          )
+                            ? "bold"
+                            : "normal", // Opcional: negrita para resaltar más
+                        },
+                      }}
+                    />
+                  </Grid>
                 ))}
-              </TextField>
+              </Grid>
+            </CardContent>
+          </Card>
+          <Grid container justifyContent="center">
+            <Grid item xs={12} md={10} lg={8}>
+              <Card
+                sx={{ mb: 4, justifyContent: "center", alignItems: "center" }}
+              >
+                <CardHeader
+                  title="Horarios de Atención"
+                  titleTypographyProps={{ variant: "h6" }}
+                  sx={{
+                    bgcolor: "primary.main",
+                    color: "white",
+                    textAlign: "center",
+                  }}
+                />
+                <CardContent>
+                  <Grid
+                    container
+                    spacing={2}
+                    sx={{
+                      mb: 2,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Grid
+                      item
+                      xs={12}
+                      md={3}
+                      sx={{ justifyContent: "center", alignItems: "center" }}
+                    >
+                      <TextField
+                        select
+                        fullWidth
+                        label="Día"
+                        name="id_dia"
+                        value={nuevoHorario.id_dia}
+                        onChange={handleHorarioChange}
+                        required
+                      >
+                        <MenuItem value="">Seleccione día</MenuItem>
+                        {diasDisponibles.map((dia) => (
+                          <MenuItem key={dia.id} value={dia.id}>
+                            {dia.nombre_dia}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                      <TextField
+                        fullWidth
+                        label="Hora Inicio"
+                        type="time"
+                        name="hora_desde"
+                        value={nuevoHorario.hora_desde}
+                        onChange={handleHorarioChange}
+                        InputLabelProps={{ shrink: true }}
+                        inputProps={{
+                          min: "07:00",
+                          max: "18:00", //step: 1800 Opcional: incrementos de 30 minutos (1800 segundos)
+                        }}
+                        required
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                      <TextField
+                        fullWidth
+                        label="Hora Fin"
+                        type="time"
+                        name="hora_hasta"
+                        value={nuevoHorario.hora_hasta}
+                        onChange={handleHorarioChange}
+                        InputLabelProps={{ shrink: true }}
+                        inputProps={{
+                          min: "07:00",
+                          max: "18:00", //step: 1800 Opcional: incrementos de 30 minutos (1800 segundos)
+                        }}
+                        required
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={12}
+                      md={3}
+                      sx={{ display: "flex", alignItems: "center" }}
+                    >
+                      <Button
+                        variant="contained"
+                        startIcon={<Add />}
+                        onClick={agregarHorario}
+                        disabled={
+                          !nuevoHorario.id_dia ||
+                          !nuevoHorario.hora_desde ||
+                          !nuevoHorario.hora_hasta
+                        }
+                        fullWidth
+                      >
+                        Agregar
+                      </Button>
+                    </Grid>
+                  </Grid>
+                  {console.log("Horarios =>", horarios)}
+                  {horarios.length > 0 ? (
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Día</TableCell>
+                          <TableCell>Hora Inicio</TableCell>
+                          <TableCell>Hora Fin</TableCell>
+                          <TableCell>Acciones</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {horarios.map((horario, index) => {
+                          const dia = diasDisponibles.find(
+                            (d) => d.id === horario.id_dia
+                          );
+                          return (
+                            <TableRow key={index}>
+                              <TableCell
+                                sx={{
+                                  color: "primary.main",
+                                  fontWeight: "bold",
+                                }}
+                              >
+                                {dia?.nombre_dia || "Desconocido"}
+                              </TableCell>
+                              <TableCell
+                                sx={{
+                                  color: "primary.main",
+                                  fontWeight: "bold",
+                                }}
+                              >
+                                {horario.hora_desde}
+                              </TableCell>
+                              <TableCell
+                                sx={{
+                                  color: "primary.main",
+                                  fontWeight: "bold",
+                                }}
+                              >
+                                {horario.hora_hasta}
+                              </TableCell>
+                              <TableCell>
+                                <IconButton
+                                  color="error"
+                                  onClick={() => eliminarHorario(index)}
+                                  title="Eliminar horario"
+                                >
+                                  <Delete />
+                                </IconButton>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                      </TableBody>
+                    </Table>
+                  ) : (
+                    <Alert severity="info">No hay horarios asignados</Alert>
+                  )}
+                </CardContent>
+              </Card>
             </Grid>
-            <Grid item xs={12} md={3}>
-              <TextField
-                fullWidth
-                label="Hora Inicio"
-                type="time"
-                name="hora_desde"
-                value={nuevoHorario.hora_desde}
-                onChange={handleHorarioChange}
-                InputLabelProps={{ shrink: true }}
-                required
-              />
-            </Grid>
-            <Grid item xs={12} md={3}>
-              <TextField
-                fullWidth
-                label="Hora Fin"
-                type="time"
-                name="hora_hasta"
-                value={nuevoHorario.hora_hasta}
-                onChange={handleHorarioChange}
-                InputLabelProps={{ shrink: true }}
-                required
-              />
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={3}
-              sx={{ display: "flex", alignItems: "center" }}
-            >
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid item>
               <Button
                 variant="contained"
-                startIcon={<Add />}
-                onClick={agregarHorario}
+                color="success"
+                size="large"
+                startIcon={<Save />}
+                onClick={guardarAsignacion}
                 disabled={
-                  !nuevoHorario.id_dia ||
-                  !nuevoHorario.hora_desde ||
-                  !nuevoHorario.hora_hasta
+                  cursosSeleccionados.length === 0 && horarios.length === 0
                 }
-                fullWidth
               >
-                Agregar
+                Guardar Asignación Cursos/Horario de Atención
               </Button>
             </Grid>
           </Grid>
-          {console.log("Horarios =>", horarios)}
-          {horarios.length > 0 ? (
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Día</TableCell>
-                  <TableCell>Hora Inicio</TableCell>
-                  <TableCell>Hora Fin</TableCell>
-                  <TableCell>Acciones</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {horarios.map((horario, index) => {
-                  const dia = diasDisponibles.find(
-                    (d) => d.id === horario.id_dia
-                  );
-                  return (
-                    <TableRow key={index}>
-                      <TableCell>{dia?.nombre_dia || "Desconocido"}</TableCell>
-                      <TableCell>{horario.hora_desde}</TableCell>
-                      <TableCell>{horario.hora_hasta}</TableCell>
-                      <TableCell>
-                        <IconButton
-                          color="error"
-                          onClick={() => eliminarHorario(index)}
-                          title="Eliminar horario"
-                        >
-                          <Delete />
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          ) : (
-            <Alert severity="info">No hay horarios asignados</Alert>
-          )}
-        </CardContent>
-      </Card>
-
-      <Button
-        variant="contained"
-        color="success"
-        size="large"
-        startIcon={<Save />}
-        onClick={guardarAsignacion}
-        disabled={cursosSeleccionados.length === 0 && horarios.length === 0}
-        fullWidth
-      >
-        Guardar Asignación Completa
-      </Button>
-    </Container>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 };
 
